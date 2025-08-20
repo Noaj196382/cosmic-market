1e6de1eaa265334740cd7557647f4ae1b867a1c6
Noaj196382 <noah1246@icloud.com>
2025-08-20 05:50:03 -0600

    Create api-status.js

---
+++ .github/workflows/api-status.js
@@ -0,0 +1,5 @@
+export default function handler(req, res) {
+  res.setHeader('Content-Type', 'text/plain');
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.status(200).send("200 OK");
+}
